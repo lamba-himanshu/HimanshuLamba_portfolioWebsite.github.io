0053380d23e21a300b9d08cb9ffb39297931674d
Himanshu Lamba <92923935+lamba-himanshu@users.noreply.github.com>
2024-07-24 14:04:47 +0000

    added more features

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,18 @@
+
+var typed = new Typed('.text', {
+    strings: ['<i>Front-end Developer</i>', ' Java Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+  });
+
 // Navigation bar effects on scroll
 window.addEventListener("scroll", function() {
     const header = document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
 
 
 //project section -modal
@@ -12,7 +22,7 @@ const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
 
 var modal = function (modalClick) {
     projectModals[modalClick].classList.add("active");
-}
+};
 
 moreBtns.forEach((moreBtn, i)=>{
     moreBtn.addEventListener('click', ()=> {
@@ -28,3 +38,76 @@ modalCloseBtns.forEach((modalCloseBtn)=>{
         });
     })
 });
+
+//website dark/light theme button
+
+const themeBtn = document.querySelector(".theme-btn");
+themeBtn.addEventListener('click', () =>{
+    document.body.classList.toggle("dark-theme");
+    themeBtn.classList.toggle("sun");
+
+    localStorage.setItem("saved-theme",getCurrentTheme());
+    localStorage.setItem("saved-icon",getCurrentIcon());
+});
+
+const getCurrentTheme= () => document.body.classList.contains("dark-theme")? "dark" : "light";
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" :"moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if(savedTheme){
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+}
+
+
+
+// scroll to top button
+const scrollTopBtn = document.querySelector('.scrollToTop-btn');
+const footerContainer = document.querySelector('.footer-container');
+const scrollTopArrow = scrollTopBtn.querySelector('i');
+// Show the scroll-to-top button when scrolling down
+window.addEventListener("scroll", function() {
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+// Change color of the scroll-to-top button when it intersects with the footer
+const observer = new IntersectionObserver(function(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            scrollTopBtn.style.backgroundColor = "#fff";
+            scrollTopArrow.style.color = "#6a59d1";
+        } else {
+            scrollTopBtn.style.backgroundColor = "#6a59d1"; // Reset to original color or set another color
+            scrollTopArrow.style.color = "#fff";
+        }
+    });
+}, { threshold: 0.5 }); // Adjust the threshold as needed
+
+observer.observe(footerContainer);
+
+
+scrollTopBtn.addEventListener("click", function(){
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+});
+
+// Navigation menu item active on page scroll
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.scrollY; // Use window.scrollY instead of window.pageYOffset
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let id = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+        } else {
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+        }
+    });
+});
+
